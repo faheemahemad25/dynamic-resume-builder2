71d91948aa1237ff4928e6c789a24d8c996f144f
Faheem Ahemad <faheemahemad25@gmail.com>
2025-02-05 22:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-ick', ()=>{
-
-    if(barIcon.cst resumeCategoryIsopened = document.querySelector('.resumeCategoryIsopened');
-const resumeCatmeCategoryMenu');
-
-resumeCategoryIsopened.addEventListener('click', (e)=>{
-    
-   
-    if(resumeCategoryMenu.classList.contains('hidden')){
-   
-
-
-function movetoDestination(){
-    mobileNavBar.classList.add('hidden');
-    barIcon.classList.remove('fa-xmark')
-}
-
-
-
-const resumslideControllerY(count)
-
 
 //📗🔖 function run automatically like in css animation and @keyframes{} same in javascript setInterval()
 (() => {
@@ -28,60 +7,16 @@ const resumslideControllerY(count)
    
     if (window.matchMedia('(max-width: 768px)').matches) { 
     
-
 // Stop event propagation.   when child click then parent eventlistner runs. so we dont want when child click then parent event fires.
 resumeCategoryMenu.addEventListener('click',(e)=>{
    e.stopPropagation();
 })
 
-
-
-
-ySelector('.backBtn')
-
-nextButton.addEventListener('click', ()=>{
-    const scrollAmount = scrollContainer.offsetWidth;
-    console.log(scrollAmount); 
-     
-    //🔖📗  ---
-        scrollContainer.scrollLeft = scrollContainer.scrollLeft + scrollAmount / 4; //🔖📗 firstly and mainly this execute after reaching at last then if condition line of code execute.
-    } 
-
-})
-
-backButton.addEventListener('click', ()=>{
-    const scrollAmount = scrollContainer.offsetWidth;
-    console.log(scrollAmount);
-    //🔖📗  ----t = scrollContainer.scrollLeft - scrollAmount / 4;
-  n css animation and @keyframes{} same in javascript setInterval()
-setInterval(() => {
-    slidexController(1); 
-}, 20888800);crllContainer.scrollLeft",scrollContainer.scrollLeft);
-    
-   rollLeft = scrollContainer.scrol
-            scrollContainer.scrollLeft = scrollContainer.scrollLeft + scrollAmount / 4; //🔖📗 firstly and mainly this execute after reaching at last then if condition line of code execute.
-
-        }    
-    }
-}
-sslideControllerY(count)
-
-
-//📗🔖 function run automatically like in css animation and @keyframes{} same in javascript setInterval()
 setInterval(() => {
     slidexController(1); 
 }, 20888800);oScrollMove();
 },4000)
 
-
-
-
-
-
-
-
-
-//  =============================================================================================== 
 /ory)=>{
         category.style.display="none";
     })
