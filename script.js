d07349e426660e16c08487e266ae86ed68013edc
Faheem Ahemad <faheemahemad25@gmail.com>
2025-02-05 22:40:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ resumeCategoryMenu.addEventListener('click',(e)=>{
    e.stopPropagation();
 })
 
+if (window.matchMedia('(max-width: 768px)').matches) {  
+// Stop event propagation.  
+resumeCategoryMenu.addEventListener('click',(e)=>{
+   e.stopPropagation();
+})
+
 // Hide all cards initially
 for(let card of cards){
     count = count + numb;
