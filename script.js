eadc76adf72b28e154bbc7d908c520b1972694a2
Faheem Ahemad <faheemahemad25@gmail.com>
2025-02-05 22:38:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-
-//📗🔖 function run automatically like in css animation and @keyframes{} same in javascript setInterval()
-(() => {
-    slidexController(1); 
-}, 20888800);lAmount); 
-     
-   
-    if (window.matchMedia('(max-width: 768px)').matches) { 
+  
+if (window.matchMedia('(max-width: 768px)').matches) { 
     
 // Stop event propagation.   when child click then parent eventlistner runs. so we dont want when child click then parent event fires.
 resumeCategoryMenu.addEventListener('click',(e)=>{
@@ -17,30 +11,9 @@ setInterval(() => {
 }, 20888800);oScrollMove();
 },4000)
 
-/ory)=>{
-        category.style.display="none";
-    })
-
-    // document.getElementById(categoryName).style.display="block"; // to be show 
-    document.getElementById(categoryName).style.display="flex"; //🔖📗
-    
-}
-displayCategory('All-templates'); //🔖📗 Trigger the default category display instead of giving default
-
-
-
-
-c
-    
-});
-
-/
-
 // Hide all cards initially
 // 📗🔖
 for(let card of cards){
-
-
     count = count + numb;
     console.log(count);
    
@@ -56,10 +29,6 @@ for(let card of cards){
 slideControllerY(count)
 
 
-//📗🔖 function run automatically like in css animation and @keyframes{} same in javascript setInterval()
-setInterval(() => {
-    slidexController(1); 
-}, 20888800);
 
 
 
